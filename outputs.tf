cdec1f321cb4295f85c0c6cefcd417d044aee1cf
Simon Rance <sirance@gmail.com>
2025-01-31 08:04:19 +0000

    feat: removed vm code, added oke cluster & node pool

--- outputs.tf
+++ outputs.tf
@@ -1,65 +1,99 @@
-# Output the "list" of all availability domains.
-output "all-availability-domains-in-your-tenancy" {
-  value = data.oci_identity_availability_domains.ads.availability_domains
+# # Output the "list" of all availability domains.
+# output "all-availability-domains-in-your-tenancy" {
+#   value = data.oci_identity_availability_domains.ads.availability_domains
+# }
+
+# # Outputs for the vcn module
+
+# output "vcn_id" {
+#   description = "OCID of the VCN that is created"
+#   value       = module.vcn.vcn_id
+# }
+# output "id-for-route-table-that-includes-the-internet-gateway" {
+#   description = "OCID of the internet-route table. This route table has an internet gateway to be used for public subnets"
+#   value       = module.vcn.ig_route_id
+# }
+# output "nat-gateway-id" {
+#   description = "OCID for NAT gateway"
+#   value       = module.vcn.nat_gateway_id
+# }
+# output "id-for-for-route-table-that-includes-the-nat-gateway" {
+#   description = "OCID of the nat-route table - This route table has a nat gateway to be used for private subnets. This route table also has a service gateway."
+#   value       = module.vcn.nat_route_id
+# }
+
+# # Outputs for private security list
+
+# output "private-security-list-name" {
+#   value = oci_core_security_list.private-security-list.display_name
+# }
+# output "private-security-list-OCID" {
+#   value = oci_core_security_list.private-security-list.id
+# }
+
+# # Outputs for public security list
+
+# output "public-security-list-name" {
+#   value = oci_core_security_list.public-security-list.display_name
+# }
+# output "public-security-list-OCID" {
+#   value = oci_core_security_list.public-security-list.id
+# }
+
+# # Outputs for private subnet
+
+# output "private-subnet-name" {
+#   value = oci_core_subnet.vcn-private-subnet.display_name
+# }
+# output "private-subnet-OCID" {
+#   value = oci_core_subnet.vcn-private-subnet.id
+# }
+
+# # Outputs for public subnet
+
+# output "public-subnet-name" {
+#   value = oci_core_subnet.vcn-public-subnet.display_name
+# }
+# output "public-subnet-OCID" {
+#   value = oci_core_subnet.vcn-public-subnet.id
+# }
+
+# # Outputs for compute instance
+
+# output "public-ip-for-compute-instance" {
+#   value = oci_core_instance.ubuntu_instance.public_ip
+# }
+
+
+# Outputs for k8s cluster
+
+output "cluster-name" {
+  value = oci_containerengine_cluster.oke-cluster.name
+}
+output "cluster-OCID" {
+  value = oci_containerengine_cluster.oke-cluster.id
+}
+output "cluster-kubernetes-version" {
+  value = oci_containerengine_cluster.oke-cluster.kubernetes_version
+}
+output "cluster-state" {
+  value = oci_containerengine_cluster.oke-cluster.state
 }
 
-# Outputs for the vcn module
+# Outputs for k8s node pool
 
-output "vcn_id" {
-  description = "OCID of the VCN that is created"
-  value       = module.vcn.vcn_id
+output "node-pool-name" {
+  value = oci_containerengine_node_pool.oke-node-pool.name
 }
-output "id-for-route-table-that-includes-the-internet-gateway" {
-  description = "OCID of the internet-route table. This route table has an internet gateway to be used for public subnets"
-  value       = module.vcn.ig_route_id
+output "node-pool-OCID" {
+  value = oci_containerengine_node_pool.oke-node-pool.id
 }
-output "nat-gateway-id" {
-  description = "OCID for NAT gateway"
-  value       = module.vcn.nat_gateway_id
+output "node-pool-kubernetes-version" {
+  value = oci_containerengine_node_pool.oke-node-pool.kubernetes_version
 }
-output "id-for-for-route-table-that-includes-the-nat-gateway" {
-  description = "OCID of the nat-route table - This route table has a nat gateway to be used for private subnets. This route table also has a service gateway."
-  value       = module.vcn.nat_route_id
+output "node-size" {
+  value = oci_containerengine_node_pool.oke-node-pool.node_config_details[0].size
 }
-
-# Outputs for private security list
-
-output "private-security-list-name" {
-  value = oci_core_security_list.private-security-list.display_name
-}
-output "private-security-list-OCID" {
-  value = oci_core_security_list.private-security-list.id
-}
-
-# Outputs for public security list
-
-output "public-security-list-name" {
-  value = oci_core_security_list.public-security-list.display_name
-}
-output "public-security-list-OCID" {
-  value = oci_core_security_list.public-security-list.id
-}
-
-# Outputs for private subnet
-
-output "private-subnet-name" {
-  value = oci_core_subnet.vcn-private-subnet.display_name
-}
-output "private-subnet-OCID" {
-  value = oci_core_subnet.vcn-private-subnet.id
-}
-
-# Outputs for public subnet
-
-output "public-subnet-name" {
-  value = oci_core_subnet.vcn-public-subnet.display_name
-}
-output "public-subnet-OCID" {
-  value = oci_core_subnet.vcn-public-subnet.id
-}
-
-# Outputs for compute instance
-
-output "public-ip-for-compute-instance" {
-  value = oci_core_instance.ubuntu_instance.public_ip
+output "node-shape" {
+  value = oci_containerengine_node_pool.oke-node-pool.node_shape
 }
